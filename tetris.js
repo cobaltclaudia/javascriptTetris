41d5b6aa7fa1035710a2823c424cce8756cbae70
claudia <ceruleancee@gmail.com>
2023-03-15 23:56:32 -0500

    Revert

--- tetris.js
+++ tetris.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBin = document.queryCommandIndeterm('#start-button');
     const width = 10;
 
-    //    console.log(squares);
+    console.log(squares);
 
     // the tetrominoes
     const tetrominoL = [
@@ -20,48 +20,39 @@ document.addEventListener('DOMContentLoaded', () => {
         [0, width, width + 1, width * 2 + 1],
         [width + 1, width + 2, width * 2, width * 2 + 1]
     ]
-
     const tetrominoT = [
         [1, width, width + 1, width + 2],
         [1, width + 1, width + 2, width * 2 + 1],
         [width, width + 1, width + 2, width * 2 + 1],
         [1, width, width + 1, width * 2 + 1]
     ]
-
     const tetrominoO = [
         [0, 1, width, width + 1],
         [0, 1, width, width + 1],
         [0, 1, width, width + 1],
         [0, 1, width, width + 1]
     ]
-
     const tetrominoI = [
         [1, width + 1, width * 2 + 1, width * 3 + 1],
         [width, width + 1, width + 2, width + 3],
         [1, width + 1, width * 2 + 1, width * 3 + 1],
         [width, width + 1, width + 2, width + 3]
-    ]
+    ];
 
-    const theTetrominoes = [tetrominoI, tetrominoL, tetrominoO, tetrominoT, tetrominoZ]
+    const theTetrominoes = [tetrominoL, tetrominoZ, tetrominoT, tetrominoO, tetrominoI];
+
     let currentPosition = 4;
-    let currentRotation = 0;
+    let current = theTetrominoes[0][0];
 
-    // randomly select a tetromino at its first rotation
-    let random = Math.floor(Math.random() * theTetrominoes.length);
-    let current = theTetrominoes[random][currentRotation];
+    console.log(theTetrominoes[0][0]);
 
     // draw the tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino');
-        })
+        });
     }
 
-    //undraw the tetromino
-    function undraw() {
-        current.forEach(index => {
-            square[currentPosition + index].classList.remove('tetromino');
-        })
-    }
+    draw();
 
-})
+})// addEventListener brackets
